1973c4d8c125f0b0552faf9354495ffcada11022
QuantumEPR <stevenzheng000131@gmail.com>
2022-03-09 11:29:34 -0800

    fix entry point attempt 1

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-theme-kb`,
       options: {
-        rootNote: "/init",
+        rootNote: "/Learning%20Statement",
         contentPath: `${__dirname}/..`,
         ignore: [
           "**/_layouts/**",
